7becf36db51409884a9bb811e416500d7067116b
whatwereyouthinking <karlronsaria@live.com>
2023-10-08 05:47:56 -0700

    feat(Remove-Item) fireball sfx

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Sources
+### Sounds
+- fireball.wav
+  - YouTube
+    - title: Battle sfx [fire ball]
+    - uploader: GAS
+  - url: https://www.youtube.com/watch?v=7h2dRFBfHvo
+  - retrieved: 2023_10_08
+- oh-no-our-table.wav
+  - various
+  - search: oh no our table
--- script/Annoying.ps1
+++ script/Annoying.ps1
@@ -85,6 +85,126 @@ function Write-Output {
 #>
 }
 
+function Remove-Item {
+    [CmdletBinding(
+        DefaultParameterSetName = 'Path',
+        SupportsShouldProcess = $true,
+        ConfirmImpact = 'Medium',
+        SupportsTransactions = $true,
+        HelpUri = 'https://go.microsoft.com/fwlink/?LinkID=113373'
+    )]
+    param(
+        [Parameter(
+            ParameterSetName = 'Path',
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [string[]]
+        ${Path},
+
+        [Parameter(
+            ParameterSetName = 'LiteralPath',
+            Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [Alias('PSPath')]
+        [string[]]
+        ${LiteralPath},
+
+        [string]
+        ${Filter},
+
+        [string[]]
+        ${Include},
+
+        [string[]]
+        ${Exclude},
+
+        [switch]
+        ${Recurse},
+
+        [switch]
+        ${Force},
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [pscredential]
+        [System.Management.Automation.CredentialAttribute()]
+        ${Credential}
+    )
+
+    dynamicparam
+    {
+        try {
+            $targetCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Management\Remove-Item', [System.Management.Automation.CommandTypes]::Cmdlet, $PSBoundParameters)
+            $dynamicParams = @($targetCmd.Parameters.GetEnumerator() | Microsoft.PowerShell.Core\Where-Object { $_.Value.IsDynamic })
+            if ($dynamicParams.Length -gt 0)
+            {
+                $paramDictionary = [Management.Automation.RuntimeDefinedParameterDictionary]::new()
+                foreach ($param in $dynamicParams)
+                {
+                    $param = $param.Value
+
+                    if(-not $MyInvocation.MyCommand.Parameters.ContainsKey($param.Name))
+                    {
+                        $dynParam = [Management.Automation.RuntimeDefinedParameter]::new($param.Name, $param.ParameterType, $param.Attributes)
+                        $paramDictionary.Add($param.Name, $dynParam)
+                    }
+                }
+                return $paramDictionary
+            }
+        } catch {
+            throw
+        }
+    }
+
+    begin
+    {
+        try {
+            $outBuffer = $null
+            if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer))
+            {
+                $PSBoundParameters['OutBuffer'] = 1
+            }
+            $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Management\Remove-Item', [System.Management.Automation.CommandTypes]::Cmdlet)
+            $scriptCmd = {& $wrappedCmd @PSBoundParameters }
+            $steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
+            $steppablePipeline.Begin($PSCmdlet)
+        } catch {
+            throw
+        }
+    }
+
+    process
+    {
+        try {
+            $steppablePipeline.Process($_)
+        } catch {
+            throw
+        }
+    }
+
+    end
+    {
+        try {
+            $steppablePipeline.End()
+        } catch {
+            throw
+        }
+
+        $player = New-Object System.Media.SoundPlayer
+        $player.SoundLocation =
+            dir "$PsScriptRoot/../res/fireball.wav"
+        $player.Play()
+    }
+
+<#
+.ForwardHelpTargetName Microsoft.PowerShell.Management\Remove-Item
+.ForwardHelpCategory Cmdlet
+#>
+}
+
 function Send-Distress {
     $player = New-Object System.Media.SoundPlayer
     $player.SoundLocation =
