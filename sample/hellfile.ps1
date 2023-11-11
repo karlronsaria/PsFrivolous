6c1c6cb0ec4557f6896826df30ca36c818b09577
karlronsaria <andrew.mikedaniels@gmail.com>
2023-11-11 03:42:00 -0800

    feat(BoxDrawnText): new font Poison

--- sample/hellfile.ps1
+++ sample/hellfile.ps1
@@ -11,12 +11,48 @@ Set-Variable `
     -Value (New-Object System.Media.SoundPlayer)
 
 $script:player.SoundLocation =
-    dir "$PsScriptRoot/Scripts/PsFrivolous/res/burning-intro.wav"
+    dir "$PsScriptRoot/Scripts/PsFrivolous/res/intro/*.wav" | Get-Random
+
 $script:player.Play()
 
 Remove-Variable `
     -Scope 'Script' `
     -Name 'player'
+
+function Write-HellfileGreeting {
+    $script:msg = @{
+        1 = @("HELL-o", "There!")
+        2 = @("THATS NO", "GOOD")
+        3 = @("Let me", "HELLp!")
+    }
+
+    Write-Host ""
+    Write-Host `
+        -ForegroundColor Red `
+        -Object $(
+            Get-BoxDrawnText `
+                -Message $(
+                    $myMessage = $script:msg[$($script:msg.Keys | Get-Random)]
+
+                    switch ($PsVersionTable.PsVersion.Major) {
+                        7 {
+                            "$myMessage"
+                        }
+                        default {
+                            $myMessage
+                        }
+                    }
+                ) `
+                -FontMap $(
+                    switch ($PsVersionTable.PsVersion.Major) {
+                        7 { "Edge" }
+                        default { "Poison" }
+                    }
+                )
+        )
+}
+
+Write-HellfileGreeting
 
 Set-PromptAnnoying
 Set-PsReadLineOption -EditMode Vi
